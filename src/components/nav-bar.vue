2bdb6ecdc6513c332bf028fe7c84dbe0d0123258
David Carrillo <david.carrillo.moncayo@gmail.com>
2024-09-11 12:01:01 -0500

    Mejoras

--- src/components/nav-bar.vue
+++ src/components/nav-bar.vue
@@ -1,24 +1,36 @@
 <script setup>
-import { computed, ref } from 'vue'; // Importado para usar propiedades computadas
-import { useRoute } from 'vue-router'; // Importado para obtener la ruta actual
-
+import { computed, ref } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
 const route = useRoute(); // Obtiene la ruta actual
+const router = useRouter(); // Obtiene el objeto router
+
+// Comprobamos si hay historial para regresar
+const canGoBack = ref(window.history.length > 1);
+
+// Función para ir a la página anterior
+const goBack = () => {
+  if (canGoBack.value) {
+    router.back();
+  }
+};
+
+// Función para activar la clase 'active' en el enlace actual
 const isActive = (path) => route.path.startsWith(path);
 </script>
 
 <template>
   <nav>
-    
-    <router-link to="/users" class="router" :class="{'active': isActive('/users')}">Usuario</router-link>
-    <router-link to="/tec" class="router" :class="{'active': isActive('/tec')}">Tecnicos</router-link>
-    <router-link to="/phones" class="router" :class="{'active': isActive('/phones')}">Celulares</router-link>
-    <router-link to="/spareparts" class="router" :class="{'active': isActive('/spareparts')}">Repuestos</router-link>
+    <router-link to="/users" class="router" :class="{ 'active': isActive('/users') }">Usuario</router-link>
+    <router-link to="/tec" class="router" :class="{ 'active': isActive('/tec') }">Tecnicos</router-link>
+    <router-link to="/phones" class="router" :class="{ 'active': isActive('/phones') }">Celulares</router-link>
+    <router-link to="/spareparts" class="router" :class="{ 'active': isActive('/spareparts') }">Repuestos</router-link>
+    <button @click="goBack" :disabled="!canGoBack" class="back-button"><ion-icon name="arrow-back-circle-outline"></ion-icon></button>
   </nav>
 </template>
 
 <style scoped>
-nav{
+nav {
   background-color: rgb(216, 75, 23);
   display: flex;
   flex-direction: column;
@@ -27,7 +39,8 @@ nav{
   height: 100%;
   width: 100%;
 }
-.router{
+
+.router {
   color: white;
   padding: 20px;
   text-decoration: none;
@@ -41,5 +54,46 @@ nav{
   transform: translateX(15px);
   border: 1px solid rgba(0, 0, 0, 0.537);
   box-shadow: -5px 5px 5px rgba(0, 0, 0, 0.418);
+}
+
+.back-button {
+  width: 9em;
+  height: 3em;
+  border-radius: 30em;
+  font-size: 35px;
+  font-family: inherit;
+  border: none;
+  position: absolute;
+  bottom: 20px;
+  left: 10px;
+  overflow: hidden;
+  z-index: 1;
+  transition: color .4s ease-in;
+  box-shadow: 0 0 10px 4px rgba(0, 0, 0, 0.611);
+}
+
+.back-button::before {
+  content: '';
+  width: 0;
+  height: 3em;
+  border-radius: 30em;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-image: linear-gradient(to right, rgb(122, 79, 0) 0%, orange 100%);
+  transition: .5s ease;
+  display: block;
+  z-index: -1;
+}
+
+.back-button:disabled {
+  background-color: grey;
+  cursor: not-allowed;
+}
+.back-button:hover::before {
+ width: 9em;
+}
+.back-button:hover{
+ color: white;
 }
 </style>
